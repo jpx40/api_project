db882d687558ac8534d8af4b8fc1a11ab58829e1
jpx40 <jonaspeitz@proton.me>
2023-09-12 13:46:05 +0200

    api

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python -m venv env
+python3 -m venv env
 
 source ./env/bin/activate
 
-python -m pip install -r requirements.txt
+#python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt --break-system-packages
